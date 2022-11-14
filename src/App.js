0276c34b90b66b2af8804133ad251b2a03c0f5ff
soumia elyakni <selyakni@gmail.com>
2022-11-14 16:24:37 +0100

    start frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import Login from './components/auth/login/login.js'
+import Auth from './components/auth/auth';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className="mb-4">Welcome to Marhaba Livraison</h3>
+
+      <Auth/>
+
     </div>
   );
 }
-
 export default App;
