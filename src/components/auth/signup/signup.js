73cc5e2c323e7006da77e9ac127d55da474b6c37
soumia elyakni <selyakni@gmail.com>
2022-11-15 18:21:37 +0100

    change for to htmlFor

--- src/components/auth/signup/signup.js
+++ src/components/auth/signup/signup.js
@@ -8,30 +8,30 @@ const Signup = ()=> {
 <form className="signup-form" action="#" method="post">
 
    <div className="form-group">
-      <label for="name">Full Name</label>
+      <label htmlFor="name">Full Name</label>
       <input type="text" name="username" id="name" className="name"/>
       <span className="error"></span>
    </div>
 
    <div className="form-group">
-      <label for="email">Email Adderss</label>
+      <label htmlFor="email">Email Adderss</label>
       <input type="email" name="emailAdress" id="email" className="email"/>
       <span className="error"></span>
    </div>
 
    <div className="form-group">
-      <label for="phone">Phone Number - <small>Optional</small></label>
+      <label htmlFor="phone">Phone Number - <small>Optional</small></label>
       <input type="text" name="phone" id="phone"/>
    </div>
 
    <div className="form-group">
-      <label for="password">Password</label>
+      <label htmlFor="password">Password</label>
       <input type="password" name="password" id="password" className="pass"/>
       <span className="error"></span>
    </div>
 
    <div className="form-group">
-      <label for="passwordCon">Confirm Password</label>
+      <label htmlFor="passwordCon">Confirm Password</label>
       <input type="password" name="passwordCon" id="passwordCon" className="passConfirm"/>
       <span className="error"></span>
    </div>
