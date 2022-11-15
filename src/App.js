8d3591d9117aa423f6bdaa322af8540d1b82d9a9
soumia elyakni <selyakni@gmail.com>
2022-11-15 11:42:40 +0100

    use react-router-dom in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-// import Login from './components/auth/login/login.js'
 import Auth from './components/auth/auth';
 import './App.css';
 function App() {
   return (
     <div className="App">
-      <h3 className="mb-4">Welcome to Marhaba Livraison</h3>
+    <BrowserRouter>
+      <Routes>
 
-      <Auth/>
-
+          <Route path="/" element={<Auth/>} />
+          <Route path="/auth" element={<Auth />} />
+          {/* <Route path="/*" element={<Error />} /> */}
+        
+      </Routes>
+    </BrowserRouter>
+      
     </div>
   );
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 export default App;
