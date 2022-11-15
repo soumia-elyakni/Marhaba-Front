a21f4833f201a86c4a42be143050423c322a74de
soumia elyakni <selyakni@gmail.com>
2022-11-15 11:39:34 +0100

    Add login form

--- src/components/auth/login/login.js
+++ src/components/auth/login/login.js
@@ -1,51 +1,47 @@
-import './login.css';
+import "./login.css"
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl, FormLabel, Form, input } from "react-bootstrap"
 
-const Login = ()=>{
+const Login = ()=> {
+ 
     return (
     <>
-      <form className="d-flex flex-column">
-        <div className="w-75 m-auto p-3">
-        
-        <h3 className="mt-3">Sign In</h3>
-        <div className="mb-3">
-          <label className="float-start ms-2">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
-        <div className="mb-3">
-          <label className="float-start ms-2">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
-        <div className="mb-3">
-          <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
-          </div>
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-success">
-            Sign In
-          </button>
-        </div>
-        <p className="forgot-password text-right mt-2">
-          Forgot <a href="#" className="text-success">password?</a>
-        </p>
-        </div>
-      </form>
+     
+     <div className="container">
+      <section id="formHolder">
+
+     
+            <div className="login form-peice switched">
+               <form className="login-form" action="#" method="post">
+                  <div className="form-group">
+                     <label for="loginemail">Email Adderss</label>
+                     <input type="email" name="loginemail" id="loginemail" required />
+                  </div>
+
+                  <div className="form-group">
+                     <label for="loginPassword">Password</label>
+                     <input type="password" name="loginPassword" id="loginPassword" required />
+                  </div>
+
+                  <div className="CTA">
+                     <input type="submit" value="Login" />
+                     <a href="/signup" className="switch">I'm New</a>
+                  </div>
+               </form>
+            </div>
+
+
+          
+      
+         
+    
+
+   </section>
+   </div>
+
+  
     </>
+
     )
 }
 
