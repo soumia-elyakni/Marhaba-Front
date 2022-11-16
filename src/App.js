aee45f3ec3a432dfae48a1e1f5e3212d205091b3
soumia elyakni <selyakni@gmail.com>
2022-11-16 09:42:49 +0100

    change router  in app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Auth from './components/auth/auth';
-// import Login from './components/auth/login/login';
-// import Signup from './components/auth/signup/signup';
+import Login from './components/auth/login/login';
+import Signup from './components/auth/signup/signup';
 import Error from './components/Error/error404';
 import './App.css';
 
@@ -12,20 +12,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
-        
-          <BrowserRouter>
-            <Routes>
-                <Route path="/auth" element={<Auth/>} />
-                {/* <Route path="/signup" element={<Signup/>} />
-                <Route path="/login" element={<Login />} /> */}
-                <Route path="/*" element={<Error />} />
-            </Routes>
-          </BrowserRouter>
-        </div>
-      
-    
-    
+
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          {/* <Route path="/*" element={<Error />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+
+
+
   );
 }
 
