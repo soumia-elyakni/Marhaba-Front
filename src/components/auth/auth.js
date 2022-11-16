f1869cc2cdc425ba6c614a4fb9c073772b421f54
soumia elyakni <selyakni@gmail.com>
2022-11-16 09:43:09 +0100

    desactive auth

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -1,51 +1,27 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Routes, Route } from "react-router-dom";
 import "./auth.css"
 import Login from "./login/login.js";
 import Signup from "./signup/signup";
-// import Error from "";
-// import Signup from "./signup/signup";
-
+import Error from "../Error/error404";
 
 
 const Auth = () => {
     return (
         <>
-
-        <div className="row">
-
-        <div className="col-sm-6 brand">
-            {/* <a href="#" class="logo">Welcome to  <span></span></a> */}
-
-            <div className="heading">
-               <h2 className="mb-3">Marhaba Livraison</h2>
-               <h3>Near to your MEAL</h3>
+            <div>
+                <div>
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
+                        {/* <Route path="/login" element={<Login />} /> */}
+                        {/* <Route path="/*" element={<Error />} /> */}
+                    </Routes>
+                </div>
             </div>
-
-            {/* <div class="success-msg">
-               <p>Great! You are one of our members now</p>
-               <a href="#" class="profile">Your Profile</a>
-            </div> */}
-         </div>
-           
-        <div className="col-sm-6 form">
-                {/* <Login/> */}
-      <Routes>
-
-          <Route path="/auth" element={<Signup />} />
-          <Route path="auth/login" element={<Login />} />
-          <Route path="auth/signup" element={<Signup />} />
-          {/* <Route path="/*" element={<Error />} /> */}
-   
-        
-      </Routes>
-        </div>
-        </div>
         </>
     )
 }
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<Auth />);
 export default Auth; 
